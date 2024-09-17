20903811fcaf92a6e8923fce5904a273c618591d
mkolisnyk <kolesnik.nickolay@gmail.com>
2024-09-17 17:34:05 +0100

    Small configuration fixes

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    defaultCommandTimeout: 20000,
     requestTimeout: 15000,
     responseTimeout: 60000,
     setupNodeEvents(on, config) {
